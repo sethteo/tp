a6c2070a5e1ae61ed466d3890ad5d74cd6d99b5a
jaejayrome <80029422+jaejayrome@users.noreply.github.com>
2024-03-26 00:52:46 +0800

    fix reading of custom addressbook from json when app launches

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -2,6 +2,7 @@ package seedu.address;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -78,11 +79,15 @@ public class MainApp extends Application {
         Optional<ReadOnlyAddressBook> addressBookOptional;
         ReadOnlyAddressBook initialAddressData;
         boolean addressBookIsPresent = false;
+
+        // TODO: RETRIEVAL OF ADDRESS BOOK FROM FILE PATH
+        Path storagePath = Paths.get("data/newAddressBook.json");
+
         try {
-            addressBookOptional = storage.readAddressBook();
+            addressBookOptional = storage.readAddressBook(storagePath);
             if (!addressBookOptional.isPresent()) {
                 logger.info("Creating a new data file " + storage.getAddressBookFilePath()
-                        + " populated with a sample AddressBook.");
+                        + " populated with custom AddressBook.");
             } else {
                 addressBookIsPresent = true;
             }
